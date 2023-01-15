cbf185cb14b557abdc5d96e0b88a7c0a78b2c7d6
Naama Shiponi <naamas1234567@gmail.com>
2023-01-15 22:32:42 +0200

    delete ptintf

--- main.c
+++ main.c
@@ -10,9 +10,7 @@ int main()
         pnode *head;
         if (c == 'A')
         {
-            printf("%c ", c);
             c = getParmsForCreatGraph(head);
-            printf("%c ", c);
 
             // printf("get %c from getParmsForCreatGraph\n",c);
             break;
@@ -45,29 +43,23 @@ char getParmsForCreatGraph(pnode *head)
 {
     int munOfNode = 0;
     scanf("%d", &munOfNode);
-    printf("%d ", munOfNode);
     // creat nodes
     build_Nodes_graph_cmd(head);
     char c;
     scanf("%c%c", &c, &c);
-    printf("%c ", c);
 
     int parentNode, targetNode, weight;
     while (c == 'n')
     {
         scanf("%d", &parentNode);
-        printf("%d ", parentNode);
 
         while (scanf("%d", &targetNode) == 1)
         {
-            printf("%d ", targetNode);
             scanf("%d", &weight);
-            printf("%d ", weight);
             // creat age
             build_graph_cmd(head);
         }
         scanf("%c", &c);
-        printf("%c ", c);
     }
     return c;
 }
@@ -75,7 +67,7 @@ char getParmsForCreatGraph(pnode *head)
 void build_Nodes_graph_cmd(pnode *head)
 {
 
-    
+
 }
 void build_graph_cmd(pnode *head)
 {
