ad9491c1616d2f0b66d11d88717cecc41850f8f8
Naama Shiponi <naamas1234567@gmail.com>
2023-01-15 22:12:00 +0200

    get number of A fun

--- graph.h
+++ graph.h
@@ -16,6 +16,7 @@ typedef struct GRAPH_NODE_ {
     struct GRAPH_NODE_ *next;
 } node, *pnode;
 
+void build_Nodes_graph_cmd(pnode *head);
 void build_graph_cmd(pnode *head);
 void insert_node_cmd(pnode *head);
 void delete_node_cmd(pnode *head);
@@ -23,5 +24,11 @@ void printGraph_cmd(pnode head); //for self debug
 void deleteGraph_cmd(pnode* head);
 void shortsPath_cmd(pnode head);
 void TSP_cmd(pnode head);
+
+char getParmsForCreatGraph();
+char getParmsForNewNode();
+char getParmsForDeleteNode();
+char getParmsForShortRoute();
+char getParmsForShortRouteWithDefinedPoints();
 
 #endif
--- main.c
+++ main.c
@@ -1,8 +1,83 @@
 #include <stdio.h>
-
+#include "graph.h"
 
 int main()
 {
-    printf("helo naama\n");
+    char c;
+    scanf("%c", &c);
+    while (1)
+    {
+        pnode *head;
+        if (c == 'A')
+        {
+            printf("%c ", c);
+            c = getParmsForCreatGraph(head);
+            printf("%c ", c);
+
+            // printf("get %c from getParmsForCreatGraph\n",c);
+            break;
+        }
+        if (c == 'B')
+        {
+            printf("in B\n");
+            // c=getParmsForNewNode();
+            break;
+        }
+        if (c == 'D')
+        {
+            // c=getParmsForDeleteNode();
+            break;
+        }
+        if (c == 'S')
+        {
+            // c=getParmsForShortRoute();
+            break;
+        }
+        if (c == 'T')
+        {
+            // c=getParmsForShortRouteWithDefinedPoints();
+            break;
+        }
+        scanf("%c", &c);
+    }
+}
+char getParmsForCreatGraph(pnode *head)
+{
+    int munOfNode = 0;
+    scanf("%d", &munOfNode);
+    printf("%d ", munOfNode);
+    // creat nodes
+    build_Nodes_graph_cmd(head);
+    char c;
+    scanf("%c%c", &c, &c);
+    printf("%c ", c);
+
+    int parentNode, targetNode, weight;
+    while (c == 'n')
+    {
+        scanf("%d", &parentNode);
+        printf("%d ", parentNode);
+
+        while (scanf("%d", &targetNode) == 1)
+        {
+            printf("%d ", targetNode);
+            scanf("%d", &weight);
+            printf("%d ", weight);
+            // creat age
+            build_graph_cmd(head);
+        }
+        scanf("%c", &c);
+        printf("%c ", c);
+    }
+    return c;
+}
+
+void build_Nodes_graph_cmd(pnode *head)
+{
+
     
+}
+void build_graph_cmd(pnode *head)
+{
+
 }
