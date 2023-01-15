7197035ed3c68a43ef78c595419fb8e10c4cd31f
BenDabush <bendab2304@gmail.com>
2023-01-15 23:42:28 +0200

    Replacing the ifs with a switch case

--- main.c
+++ main.c
@@ -8,58 +8,69 @@ int main()
     while (1)
     {
         pnode *head;
-        if (c == 'A')
+        switch(c)
         {
+        case 'A':
             c = getParmsForCreatGraph(head);
-
+            printf(" %c ", c);
             // printf("get %c from getParmsForCreatGraph\n",c);
             break;
-        }
-        if (c == 'B')
-        {
+
+        case 'B':
+
             printf("in B\n");
             // c=getParmsForNewNode();
+
+
             break;
-        }
-        if (c == 'D')
-        {
+
+        case 'D':
             // c=getParmsForDeleteNode();
             break;
-        }
-        if (c == 'S')
-        {
+
+        case 'S':
             // c=getParmsForShortRoute();
             break;
-        }
-        if (c == 'T')
-        {
+
+        case 'T':
             // c=getParmsForShortRouteWithDefinedPoints();
             break;
+
+        // operator doesn't match any case constant +, -, *, /
+        // default:
+        //     printf("Error! operator is not correct");
         }
         scanf("%c", &c);
+
     }
 }
 char getParmsForCreatGraph(pnode *head)
 {
     int munOfNode = 0;
     scanf("%d", &munOfNode);
+    printf("%d ", munOfNode);
     // creat nodes
     build_Nodes_graph_cmd(head);
     char c;
     scanf("%c%c", &c, &c);
+    printf("%c ", c);
 
     int parentNode, targetNode, weight;
     while (c == 'n')
     {
         scanf("%d", &parentNode);
+        printf("%d ", parentNode);
 
         while (scanf("%d", &targetNode) == 1)
         {
+            printf("%d ", targetNode);
             scanf("%d", &weight);
+            printf("%d ", weight);
             // creat age
             build_graph_cmd(head);
         }
         scanf("%c", &c);
+        printf("%c ", c);
     }
     return c;
 }
@@ -67,7 +78,7 @@ char getParmsForCreatGraph(pnode *head)
 void build_Nodes_graph_cmd(pnode *head)
 {
 
-
+    
 }
 void build_graph_cmd(pnode *head)
 {
